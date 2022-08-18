64aa6ad5ff4c6d1585d66c0d75537f67aaea579c
Aditya Singh <saditya370@gmail.com>
2022-08-18 12:17:50 +0530

    Added Profile Page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def index 
+        @users = User.all
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+   def new 
+
+    @user = User.new
+
+   end
+   def edit 
+    @user = User.find(params[:id])
+   end
+   def update 
+ 
+    @user = User.new(users_params)
+        
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+   end
+    def create
+      #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(users_params)
+        
+      if @user.save
+        redirect_to new_user_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private 
+    def users_params
+
+        params.require(:user).permit(:username, :email,:password)
+    end
+end
